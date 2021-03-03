7e67a7dad812a995cee4e979a7312ac86e01edd7
pelund <pelund@wpi.edu>
2021-03-03 16:02:46 -0500

    Improved GUI Layout
    GUI now consists of uniform buttons

--- TestingGui.py
+++ TestingGui.py
@@ -64,34 +64,34 @@ def showGreyscale():
 def clearCImg():
     global render
     render = ''
-    img.configure(image=render)
+    #img.configure(image=render)
 
 
 def clearGImg():
     global gRender
     gRender = ''
-    img.configure(image=gRender)
+    #img.configure(image=gRender)
 
 
 window.title('Testing Gui')
-window.geometry("1920x1080")
+window.geometry("1900x1080")
 window.config(background="white")
 
-label_file_explorer = tk.Label(window, text="File Explorer using Tkinter", width=100, height=4, fg="blue")
+label_file_explorer = tk.Label(window, text="The color will appear here once calculated", width=135, height=4, fg="blue")
 
-button_explore = tk.Button(window, text="Browse Files", command=browseFiles)
+button_explore = tk.Button(window, text="Browse Files", command=browseFiles,width=135, height=4)
 
-button_exit = tk.Button(window, text="Exit", command=exit)
+button_exit = tk.Button(window, text="Exit", command=exit,width=135, height=4, bg = "red")
 
-button_color = tk.Button(window, text="Obtain Color", command=getColor)
+button_color = tk.Button(window, text="Obtain Color", command=getColor, width=135, height=4)
 
-button_show_greyscale = tk.Button(window, text="Press to show greyscale image", command=showGreyscale)
+button_show_greyscale = tk.Button(window, text="Press to show greyscale image", command=showGreyscale, width=135, height=4)
 
-button_clear_img = tk.Button(window, text="clear color img", command=clearCImg)
+button_clear_img = tk.Button(window, text="clear color img", command=clearCImg, width=135, height=4, bg = 'orange')
 
-button_clear_g_img = tk.Button(window, text="clear greyscale img", command=clearGImg)
+button_clear_g_img = tk.Button(window, text="clear greyscale img", command=clearGImg, width=135, height=4, bg = "orange")
 
-imageColor = tk.Label(text="Welcome to Testing GUI")
+imageColor = tk.Label(text="Welcome to Testing GUI", width = 135, height = 4)
 
 imageColor.grid(column=1, row=1)
 
@@ -99,23 +99,23 @@ label_file_explorer.grid(column=1, row=2)
 
 button_explore.grid(column=1, row=3)
 
-button_exit.grid(column=1, row=4)
+button_exit.grid(column=2, row=1)
 
-button_color.grid(column=1, row=6)
+button_color.grid(column=1, row=4)
 
-button_clear_img.grid(column = 2, row = 1)
+button_clear_img.grid(column = 2, row = 2)
 
-button_clear_g_img.grid(column = 2, row = 2)
+button_clear_g_img.grid(column = 2, row = 3)
 
 img = tk.Label(image=render)
 img.image = render
-img.grid(column=1, row=7)
+img.grid(column=1, row=6)
 
-button_show_greyscale.grid(column=1, row=8)
+button_show_greyscale.grid(column=2, row=4)
 
 gimg = tk.Label(image=gRender)
 gimg.image = gRender
-gimg.grid(column=1, row=9)
+gimg.grid(column=2, row=6)
 
 # to show the window
 window.mainloop()
