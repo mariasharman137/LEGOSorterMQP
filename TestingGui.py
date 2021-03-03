808e1991049d15a109cd2635975885952e69cb81
pelund <pelund@wpi.edu>
2021-03-03 15:34:28 -0500

    Added ability to clear images
    Added buttons which clear color and greyscale images

--- TestingGui.py
+++ TestingGui.py
@@ -13,6 +13,7 @@ load = Image.open(filename)
 render = ImageTk.PhotoImage(load)
 gRender = ""
 
+
 def browseFiles():
     global filename
     global window
@@ -22,8 +23,7 @@ def browseFiles():
     label_file_explorer.configure(text="File Opened: " + filename)
     load = Image.open(filename)
     render = ImageTk.PhotoImage(load)
-    img.configure(image = render)
-
+    img.configure(image=render)
 
 
 def getColor():
@@ -34,21 +34,22 @@ def getColor():
     print(resulting_color)
     window.mainloop()
 
+
 def showGreyscale():
     global gRender
-    #localArray = PhI.photoToArray(filename)
-    #localGArray = Greyscale().ArrayToGreyscale(localArray)
-    #gImage = Image.fromarray(localGArray,'L')
-    #gRender = ImageTk.PhotoImage(gImage)
-    #img.configure(image=gRender)
+    # localArray = PhI.photoToArray(filename)
+    # localGArray = Greyscale().ArrayToGreyscale(localArray)
+    # gImage = Image.fromarray(localGArray,'L')
+    # gRender = ImageTk.PhotoImage(gImage)
+    # img.configure(image=gRender)
 
-    #gets image file, makes it into an array
+    # gets image file, makes it into an array
     testimg = PhI.photoToArray(filename)
 
-    #changes Array from a 3d RGB array into a 2d Greyscale one
+    # changes Array from a 3d RGB array into a 2d Greyscale one
     tgs = Greyscale.ArraytoGreyscale(testimg)
 
-    #makes an image from the array
+    # makes an image from the array
     gImage = Image.fromarray(tgs)
     gRender = ImageTk.PhotoImage(gImage)
     gimg.configure(image=gRender)
@@ -58,9 +59,20 @@ def showGreyscale():
         print()
 
 
+def clearCImg():
+    global render
+    render = ''
+    img.configure(image=render)
+
+
+def clearGImg():
+    global gRender
+    gRender = ''
+    img.configure(image=gRender)
+
 
 window.title('Testing Gui')
-window.geometry("2000x2000")
+window.geometry("1920x1080")
 window.config(background="white")
 
 label_file_explorer = tk.Label(window, text="File Explorer using Tkinter", width=100, height=4, fg="blue")
@@ -71,17 +83,13 @@ button_exit = tk.Button(window, text="Exit", command=exit)
 
 button_color = tk.Button(window, text="Obtain Color", command=getColor)
 
-button_show_greyscale = tk.Button(window, text = "Press to show greyscale image", command = showGreyscale)
+button_show_greyscale = tk.Button(window, text="Press to show greyscale image", command=showGreyscale)
 
-imageColor = tk.Label(text="Hello, Tkinter")
+button_clear_img = tk.Button(window, text="clear color img", command=clearCImg)
 
-img = tk.Label(image=render)
-img.image = render
-img.grid(column=1, row=7)
+button_clear_g_img = tk.Button(window, text="clear greyscale img", command=clearGImg)
 
-gimg = tk.Label(image=gRender)
-gimg.image = gRender
-gimg.grid(column=1, row=9)
+imageColor = tk.Label(text="Welcome to Testing GUI")
 
 imageColor.grid(column=1, row=1)
 
@@ -93,9 +101,19 @@ button_exit.grid(column=1, row=4)
 
 button_color.grid(column=1, row=6)
 
-button_show_greyscale.grid(column=1, row = 8)
+button_clear_img.grid(column = 2, row = 1)
 
+button_clear_g_img.grid(column = 2, row = 2)
 
+img = tk.Label(image=render)
+img.image = render
+img.grid(column=1, row=7)
+
+button_show_greyscale.grid(column=1, row=8)
+
+gimg = tk.Label(image=gRender)
+gimg.image = gRender
+gimg.grid(column=1, row=9)
 
 # to show the window
 window.mainloop()
